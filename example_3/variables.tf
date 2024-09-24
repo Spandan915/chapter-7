33caa1a14c2bcffcf5f2fa98da56c8ea4fceba11
Spandan915 <150399314+Spandan915@users.noreply.github.com>
2024-09-24 12:18:14 +0530

    Add files via upload

---
+++ example_3/variables.tf
@@ -0,0 +1,13 @@
+# Sets global variables for this Terraform project.
+
+variable app_name {
+  default="spandan19"
+}
+
+variable location {
+  default = "eastus"
+}
+
+variable kubernetes_version {  
+  default="1.29.7"  
+}
